7b16de48436ef48daa0dbd88350aa8b2ca167af3
Lutiago <perillaluisa11@gmail.com>
2023-03-23 00:26:50 +0000

    contactlist

---
+++ src/pages/add.jsx
@@ -0,0 +1,10 @@
+import { AddContact } from "@/components/Addcontact"
+
+export default function AddContactPage  () {
+    return (
+        <div className="container">
+            <AddContact/>
+        </div>
+
+    )
+}
